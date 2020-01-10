1945d7026edac0552658b1fa1e1a8e56365a172f
fadimata tillman <fadi.tillman@gmail.com>
2020-01-10 22:42:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,6 +64,20 @@ def reduce_to_total(source_array,starting point)
 end 
 end
 
+def
+i = 0 # set up a i for the enumeration of the array
+  while i < source_array .length do # a loop for each element of array
+    # Stop enumerating and return nil if any element is
+    # >6
+    if (source_array[i] => "6")
+      return [0 ]
+    end
+    i += 1
+  end 
+  return nil
+end 
+end
+
     
   
   
