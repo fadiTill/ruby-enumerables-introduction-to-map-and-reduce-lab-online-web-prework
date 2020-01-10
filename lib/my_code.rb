27519c182f043b427bddc14e4d6c4eb67242408f
fadimata tillman <fadi.tillman@gmail.com>
2020-01-10 22:36:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,58 @@
-# My Code here....
+# My Codde
+def Source array
+    source array = [ 1,2,3,4,5,6]
+     end 
+    end
+    i += 1
+  end
+  
+  
+    
+  def i map_to_negativize (source array)
+  i = 0 # set up a i for the enumeration of the array
+  while i < source_array .length do # a loop for each element of array
+    # Stop enumerating and return nil if any element is
+    # >6
+    if (source_array[i] => "6")
+      return *-1
+    end
+    i += 1
+  end
+  return nil
+end
+
+
+    
+
+    
+  def i map_to_no_change (source array)
+  i = 0 # set up a i for the enumeration of the array
+  while i < source_array .length do # a loop for each element of array
+    # Stop enumerating and return nil if any element is
+    # >6
+    if (source_array[i] => "6")
+      return  = i
+    end
+  return nil
+end
+  
+ def i map_to_square (source array)
+  i = 0 # set up a i for the enumeration of the array
+  while i < source_array .length do # a loop for each element of array
+    # Stop enumerating and return nil if any element is
+    # >6
+    if (source_array[i] => "6")
+      return **i
+    end
+    i += 1
+  end 
+  return nil
+end 
+end
+
+
+    
+  
+  
+ 
+   
