64ea0a4ec1da2de215edda5a75a27388baf2fc00
fadimata tillman <fadi.tillman@gmail.com>
2020-01-10 22:45:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -70,11 +70,11 @@ def reduce_to_any_true(source_array)
     # Stop enumerating and return nil if any element is
     # >6
     if (source_array[i] => "6")
-      return [0 ]
+      return reduce_to_any_true
     end
     i += 1
   end 
-  return nil
+  return false
 end 
 end
 
